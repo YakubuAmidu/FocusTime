3ccfe0c643ae9a8b571a4b8108b9d0a753f5d7fb
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-17 06:31:14 -0700

    Initial commit

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 export const Focus = () => {
   return (
     <View style={styles.container}>
-      <Text>What would you like to focus on?</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>What would you like to focus on?</Text>
+        <View style={styles.inputContainer}>
+          <TextInput />
+        </View>
+      </View>
     </View>
   );
 };
@@ -12,6 +18,18 @@ export const Focus = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+  },
+  titleContainer: {
+    flex: 0.5,
+    padding: 16,
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  inputContainer: {
+    paddingTop: 20,
   },
 });
