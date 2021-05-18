69b51876db3ded4ec3b021948d289273685d3e0b
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-18 01:18:35 -0700

    Initial commit

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 import { colors } from '../../utils/colors';
 import { spacing } from '../../utils/sizes';
 import { CountDown } from '../../components/CountDown';
+import { RoundedButton } from '../../components/RoundedButton';
 
 export const Timer = ({ focusSubject }) => {
+  const [isStarted, setIsStarted] = useState(false);
+
   return (
     <View style={styles.container}>
       <View style={styles.countDown}>
-        <CountDown />
+        <CountDown isPaused={!isStarted} />
       </View>
       <View style={{ paddingTop: spacing.xxl }}>
         <Text style={styles.title}>Focusing on:</Text>
         <Text style={styles.task}>{focusSubject}</Text>
       </View>
+      <RoundedButton
+        title='start'
+        size={50}
+        onPress={() => setIsStarted(true)}
+      />
     </View>
   );
 };
